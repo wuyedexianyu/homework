7df4a3cf449fa3d9084f90a1bec68fd02be2459c
fzj <90753839+wuyedexianyu@users.noreply.github.com>
2021-10-29 21:58:41 +0800

    Add files via upload

---
+++ 作业一.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int check_num(int x, int num[5])
+{
+	int a = 0, b = 0, i, A = 0;
+	for (i = 0; i < 5; i++)
+		a += num[i];
+	for (i = 0; i < 5; i++)
+		b += num[i] * num[i];
+	for (i = 2; i < x; i++)
+		if (x % i == 0)
+			A++;
+	for (i = 2; i < a; i++)
+		if (a % i == 0)
+			A++;
+	for (i = 2; i < b; i++)
+		if (b % i == 0)
+			A++;
+	return A;
+}
+void splitNum(int x, int num[5])
+{
+	int i = 0;
+	while (x != 0)
+	{
+		num[i++] = x % 10;
+		x /= 10;
+	}
+}
+int main()
+{
+	int i, n, num[5], check = 0;
+	for (n = 100; n <= 10000; n++)
+	{
+		check = 0;
+		for (i = 0; i < 5; i++)
+			num[i] = 0;
+		splitNum(n, num);
+		check = check_num(n, num);
+		if (check == 0)
+			printf("%d\n", n);
+	}
+	return 0;
+}
---
+++ 作业二.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+	int p, q, i, a = 1, b = 1, num = 0, A;
+	printf("    p     q\n");
+	for (p = 1; p <= 1000; p++)
+	{
+		for (q = 1; q <= 1000; q++)
+		{
+			a = 1;
+			b = 1;
+			A = 0;
+			num = 0;
+			for (i = 0; i < q; i++)
+				a *= p;
+			for (i = 0; i < p; i++)
+				b *= q;
+			num = p + q;
+			for (i = 2; i < num; i++)
+			{
+				if (num % i == 0)
+					A++;
+			}
+			if (A == 0)
+				printf("%5d %5d\n", p, q);
+		}
+	}
+}
