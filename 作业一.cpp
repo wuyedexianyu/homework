66e64c8a30c8fb5fcf50261a87b27d3bb635f975
fzj <90753839+wuyedexianyu@users.noreply.github.com>
2021-10-30 17:38:46 +0800

    Add files via upload

--- 作业一.cpp
+++ 作业一.cpp
@@ -28,7 +28,8 @@ void splitNum(int x, int num[5])
 }
 int main()
 {
-	int i, n, num[5], check = 0;
+	int i, n, num[5], check = 0, sum = 0, k = 0;
+	float average;
 	for (n = 100; n <= 10000; n++)
 	{
 		check = 0;
@@ -37,7 +38,13 @@ int main()
 		splitNum(n, num);
 		check = check_num(n, num);
 		if (check == 0)
+		{
 			printf("%d\n", n);
+			sum += n;
+			k++;
+		}
 	}
+	average = sum / k;
+	printf("average: %f", average);
 	return 0;
 }
